fa44b729d83b81c3c46a91139485c8c4f745b56f
AbdulhakimZatar <hakimzatar@gmail.com>
2020-10-20 16:40:54 +0300

    fix test

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -9,14 +9,14 @@ describe('API Server', () => {
   describe('Categaory routes', () => {
     let obj = { 'name': 'test0', 'display_name': 'test0', 'description': 'test test0' };
     let newObj = { 'name': '99', 'display_name': '99', 'description': '99' };
-    it('/categories should respond with 200', async () => {
-      await mockRequest.get('/categories').then((results) => {
+    it('/categories should respond with 200',() => {
+      return mockRequest.get('/categories').then((results) => {
         expect(results.status).toBe(200);
       });
     });
-    it('GET /categories/:id should respond with 200', async () => {
-      await mockRequest.post('/categories').send(obj).then((data) => {
-        mockRequest.get(`/categories/${data.body._id}`).then((results) => {
+    it('GET /categories/:id should respond with 200',() => {
+      return mockRequest.post('/categories').send(obj).then((data) => {
+        return mockRequest.get(`/categories/${data.body._id}`).then((results) => {
           expect(results.status).toBe(200);
           Object.keys(obj).forEach((key) => {
             expect(obj[key]).toEqual(results.body[0][key]);
@@ -24,17 +24,17 @@ describe('API Server', () => {
         });
       });
     });
-    it('POST /categories should respond with 201', async () => {
-      await mockRequest.post('/categories').send(obj).then((results) => {
+    it('POST /categories should respond with 201',() => {
+      return mockRequest.post('/categories').send(obj).then((results) => {
         expect(results.status).toBe(201);
         Object.keys(obj).forEach((key) => {
           expect(obj[key]).toEqual(results.body[key]);
         });
       });
     });
-    it('PUT /categories/:id should respond with 202', async () => {
-      await mockRequest.post('/categories').send(obj).then((data) => {
-        mockRequest.put(`/categories/${data.body._id}`).send(newObj).then((results) => {
+    it('PUT /categories/:id should respond with 202',() => {
+      return mockRequest.post('/categories').send(obj).then((data) => {
+        return mockRequest.put(`/categories/${data.body._id}`).send(newObj).then((results) => {
           expect(results.status).toBe(202);
           Object.keys(obj).forEach((key) => {
             expect(newObj[key]).toEqual(results.body[key]);
@@ -42,9 +42,9 @@ describe('API Server', () => {
         });
       });
     });
-    it('PATCH /categories/:id should respond with 202', async () => {
-      await mockRequest.post('/categories').send(obj).then((data) => {
-        mockRequest.patch(`/categories/${data.body._id}`).send(newObj).then((results) => {
+    it('PATCH /categories/:id should respond with 202',() => {
+      return mockRequest.post('/categories').send(obj).then((data) => {
+        return mockRequest.patch(`/categories/${data.body._id}`).send(newObj).then((results) => {
           expect(results.status).toBe(202);
           Object.keys(obj).forEach((key) => {
             expect(newObj[key]).toEqual(results.body[key]);
@@ -52,9 +52,9 @@ describe('API Server', () => {
         });
       });
     });
-    it('DELETE /categories/:id should respond with 202', async () => {
-      await mockRequest.post('/categories').send(obj).then((data) => {
-        mockRequest.delete(`/categories/${data.body._id}`).then((results) => {
+    it('DELETE /categories/:id should respond with 202',() => {
+      return mockRequest.post('/categories').send(obj).then((data) => {
+        return mockRequest.delete(`/categories/${data.body._id}`).then((results) => {
           expect(results.status).toBe(202);
           expect({}).toEqual(results.body);
         });
@@ -64,13 +64,13 @@ describe('API Server', () => {
   let obj = { 'category': 'test', 'name': 'test0', 'display_name': 'test0', 'description': 'test test0' };
   let newObj = { 'category': '99', 'name': '99', 'display_name': '99', 'description': '99' };
   describe('Product routes', () => {
-    it('/products should respond with 200', async () => {
-      await mockRequest.get('/products').then((results) => {
+    it('/products should respond with 200',() => {
+      return mockRequest.get('/products').then((results) => {
         expect(results.status).toBe(200);
       });
     });
-    it('GET /products/:id should respond with 200', async () => {
-      await mockRequest.post('/products').send(obj).then((data) => {
+    it('GET /products/:id should respond with 200',() => {
+      return mockRequest.post('/products').send(obj).then((data) => {
         mockRequest.get(`/products/${data.body._id}`).then((results) => {
           expect(results.status).toBe(200);
           Object.keys(obj).forEach((key) => {
@@ -79,17 +79,17 @@ describe('API Server', () => {
         });
       });
     });
-    it('POST /products should respond with 201', async () => {
-      await mockRequest.post('/products').send(obj).then((results) => {
+    it('POST /products should respond with 201',() => {
+      return mockRequest.post('/products').send(obj).then((results) => {
         expect(results.status).toBe(201);
         Object.keys(obj).forEach((key) => {
           expect(obj[key]).toEqual(results.body[key]);
         });
       });
     });
-    it('PUT /products/:id should respond with 202', async () => {
-      await mockRequest.post('/products').send(obj).then((data) => {
-        mockRequest.put(`/products/${data.body._id}`).send(newObj).then((results) => {
+    it('PUT /products/:id should respond with 202',() => {
+      return mockRequest.post('/products').send(obj).then((data) => {
+        return mockRequest.put(`/products/${data.body._id}`).send(newObj).then((results) => {
           expect(results.status).toBe(202);
           Object.keys(obj).forEach((key) => {
             expect(newObj[key]).toEqual(results.body[key]);
@@ -97,9 +97,9 @@ describe('API Server', () => {
         });
       });
     });
-    it('PATCH /products/:id should respond with 202', async () => {
-      await mockRequest.post('/products').send(obj).then((data) => {
-        mockRequest.patch(`/products/${data.body._id}`).send(newObj).then((results) => {
+    it('PATCH /products/:id should respond with 202',() => {
+      return mockRequest.post('/products').send(obj).then((data) => {
+        return mockRequest.patch(`/products/${data.body._id}`).send(newObj).then((results) => {
           expect(results.status).toBe(202);
           Object.keys(obj).forEach((key) => {
             expect(newObj[key]).toEqual(results.body[key]);
@@ -107,11 +107,11 @@ describe('API Server', () => {
         });
       });
     });
-    it('DELETE /products/:id should respond with 202', async () => {
-      await mockRequest.post('/products').send(obj).then((data) => {
-        mockRequest.delete(`/products/${data.body._id}`).then((results) => {
+    it('DELETE /products/:id should respond with 202',() => {
+      return mockRequest.post('/products').send(obj).then((data) => {
+        return mockRequest.delete(`/products/${data.body._id}`).then((results) => {
           expect(results.status).toBe(202);
-          expect(results.body).toEqual({});
+          expect({}).toEqual(results.body);
         });
       });
     });
