c9fe342f2ecda7ff073db40d6ef5b971b1be9cb9
AbdulhakimZatar <hakimzatar@gmail.com>
2020-10-20 16:23:14 +0300

    db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const server = require('./lib/server');
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI ||  'mongodb://localhost:27017/class-08';
+const MONGODB_URI = 'mongodb+srv://heroku:heroku1337@cluster0.1i1ry.mongodb.net/apiServer?retryWrites=true&w=majority' || process.env.MONGODB_URI ||  'mongodb://localhost:27017/class-08';
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
